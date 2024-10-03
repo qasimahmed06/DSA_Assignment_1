0a3cfa3bc11e312115d612c8dcc6db92ba2b10b0
Qasim Ahmed <qasimahmed06@gmail.com>
2024-10-03 13:29:32 +0500

    Some Progress

--- problem1.cpp
+++ problem1.cpp
@@ -15,10 +15,63 @@ struct Process { // Definining a structure to represent a process
     }
 };
 
+class ProcessScheduler {
+private:
+    Process* head;
+    Process* tail;
+    int timePerCycle; // Fixed amount of CPU time to each process in each cycle
 
-// TODO a fixed amount of CPU time to each process in each cycle. 
-// TODO remaining_time will be reduced by that amount.
-// TODO remaining_time becomes 0
-// TODO process is running and its remaining time.
-// TODO Show the state of the system after each cycle.
-// TODO Optional Task
+public:
+    ProcessScheduler(int cpu) {
+        head = nullptr;
+        tail = nullptr;
+        timePerCycle = cpu;
+    }
+
+    void addProcess(string id, int execTime) {
+        Process* newProcess = new Process(id, execTime);
+
+        if (head == nullptr) {
+            head = newProcess;
+            tail = newProcess;
+            tail->next = head; // For a circular linked list
+        }
+        else {
+            tail->next = newProcess;
+            newProcess->next = head;
+            tail = newProcess;
+        }
+    }
+
+    void removeProcess(Process* remove) {
+        if (remove == head) {
+            if (head == tail) {
+                delete head;
+                head = nullptr;
+                tail = nullptr;
+            }
+            else {
+                tail->next = head->next;
+                delete head;
+                head = tail->next;
+            }
+        }
+        else {
+            Process* current = head; //! NOT SURE IF THIS WHOLE BLOCK IS CORRECT
+            while (current->next!= remove) {
+                current = current->next;
+            }
+            current->next = remove->next;
+            if (remove == tail) {
+                tail = current;
+            }
+            delete remove;
+        }
+
+}
+
+//TODO remaining_time will be reduced by that amount.
+//TODO remaining_time becomes 0
+//TODO process is running and its remaining time.
+//TODO Show the state of the system after each cycle.
+//TODO Optional Task
